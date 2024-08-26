e3487186a70aa2d346c646fe8a1ece4adb71ae4b
DongChuyenNghiep <tuan22000@student.hamk.fi>
2024-08-26 13:05:19 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import mkcert from 'vite-plugin-mkcert'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),mkcert()],
-  server:{
-    proxy:{
-      '/api': 
-      {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const plugins = [react()];
 
-      }
-    },
-    https: true 
+  if (mode === 'development') {
+    const mkcert = require('vite-plugin-mkcert').default;
+    plugins.push(mkcert());
   }
-})
+
+  return {
+    plugins,
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+        }
+      },
+      https: mode === 'development' // Only enable HTTPS in development
+    }
+  }
+});
