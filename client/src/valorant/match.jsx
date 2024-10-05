391220c1fc5a1f0dfd2ebd34dfde8ce8eef17996
DongChuyenNghiep <tuan22000@student.hamk.fi>
2024-10-05 14:41:33 +0300

    update stat match

--- client/src/valorant/match.jsx
+++ client/src/valorant/match.jsx
@@ -1,76 +1,187 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from "react";
+import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
 
-export default function MatchResult({ matchInfo, numRound, kill, error }) {
-    const [imageUrls, setImageUrls] = useState({});
-    const images = import.meta.glob('../agent/*.{png,jpg,jpeg,gif}');
+function FeatureRichTable({ matchInfo, numRound, kill, error }) {
+  const [data, setData] = useState([]);
+  const [sortColumn, setSortColumn] = useState("acs");
+  const [sortDirection, setSortDirection] = useState("desc");
+  const tableRef = useRef(null);
+  const [imageUrls, setImageUrls] = useState({});
+  const images = import.meta.glob('../agent/*.{png,jpg,jpeg,gif}');
 
-    useEffect(() => {
-        const loadImageUrls = async () => {
-            const urls = {};
-            await Promise.all(Object.entries(images).map(async ([path, resolver]) => {
-                try {
-                    const module = await resolver();
-                    const filename = path.split('/').pop().split('.')[0];
-                    urls[filename.replace('%2F', '/')] = module.default;
-                } catch (error) {
-                    console.error(`Failed to load image at ${path}`, error);
-                }
-            }));
-            setImageUrls(urls);
-        };
-        loadImageUrls();
-    }, []);
-
-    if (error) return <div>Error: {error}</div>;
-    if (!matchInfo) return <div className="flex items-center justify-center">
-        <span className="loading loading-dots loading-lg text-primary"></span>
-    </div>;
-
-    const renderTable = (team, name) => {
-        const sortedTeam = team.sort((a, b) => {
-            const acsA = a.stats.score / numRound;
-            const acsB = b.stats.score / numRound;
-            return acsB - acsA;
-        });
-
-        return (
-            <div className='wrapper xl:flex xl:flex-col xl:gap-10 xl:items-center xl:w-full'>
-                <table border="1" cellPadding="5" className='player-stats-table table-auto max-[1024px]:w-[120%] py-2 px-1'>
-                    <thead className='all-title'>
-                        <tr>
-                            {['Player Name', "ACS", 'Kills', 'Deaths', 'Assists', "KD", "HS", 'ADR', "FK", "MK"].map((header, i) => (
-                                <th key={i} className="text-center header text-[10px] text-white first:w-[0.1%]">{header}</th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {sortedTeam.map((player, index) => (
-                            <tr key={index} className="text-center">
-                                <td className='all-title flex flex-row gap-x-2 bg-base-100'>
-                                    <img className='h-9 w-9 max-[768px]:h-8 max-[768px]:w-8' src={imageUrls[player.agent.name]} alt="Agent Icon" />
-                                    <p className='flex items-center'>{player.name}#{player.tag}</p>
-                                </td>
-                                <td className='all-title text-[10px] bg-base-100 py-2'>{(player.stats.score / numRound).toFixed(0)}</td>
-                                <td className='all-title text-[10px] py-2 px-1'>{player.stats.kills}</td>
-                                <td className='all-title text-[10px] py-2 px-1'>{player.stats.deaths}</td>
-                                <td className='all-title text-[10px] py-2 px-1'>{player.stats.assists}</td>
-                                <td className='all-title text-[10px] py-2 px-1'>{player.stats.deaths > 0 ? (player.stats.kills / player.stats.deaths).toFixed(1) : player.stats.kills}</td>
-                                <td className='all-title text-[10px] py-2 px-1'>{(player.stats.headshots * 100 / (player.stats.headshots + player.stats.bodyshots + player.stats.legshots)).toFixed(0)}%</td>
-                                <td className='all-title text-[10px] py-2 px-1'>{(player.stats.damage.dealt / numRound).toFixed(1)}</td>
-                                <td className='all-title text-[10px] py-2 px-1'>{player.fk || 0}</td>
-                                <td className='all-title text-[10px] py-2 px-1'>{player.mk || 0}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        );
+  useEffect(() => {
+    const loadImageUrls = async () => {
+      const urls = {};
+      await Promise.all(Object.entries(images).map(async ([path, resolver]) => {
+        try {
+          const module = await resolver();
+          const filename = path.split('/').pop().split('.')[0];
+          urls[filename.replace('%2F', '/')] = module.default;
+        } catch (error) {
+          console.error(`Failed to load image at ${path}`, error);
+        }
+      }));
+      setImageUrls(urls);
     };
+    loadImageUrls();
+  }, []);
 
-    return (
-        <div className="flex flex-row xl:gap-x-3 max-[1400px]:flex-col gap-y-5">
-            {renderTable(matchInfo.filter(p => p.team_id === 'Red'), 'Red')}
-            {renderTable(matchInfo.filter(p => p.team_id === 'Blue'), 'Blue')}
-        </div>
-    );
+  useEffect(() => {
+    if (matchInfo) {
+      const sortedData = [...matchInfo].sort((a, b) => {
+        const aAcs = a.stats.score / numRound;
+        const bAcs = b.stats.score / numRound;
+        if (aAcs < bAcs) return 1;
+        if (aAcs > bAcs) return -1;
+        return 0;
+      });
+      setData(sortedData);
+    }
+  }, [matchInfo, numRound]);
+
+  const handleSort = (column) => {
+    if (column === sortColumn) {
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    } else {
+      setSortColumn(column);
+      setSortDirection("asc");
+    }
+
+    const sortedData = [...data].sort((a, b) => {
+      let aValue = a;
+      let bValue = b;
+
+      if (column === "acs") {
+        aValue = a.stats.score / numRound;
+        bValue = b.stats.score / numRound;
+      } else if (column === "adr") {
+        aValue = a.stats.damage.dealt / numRound;
+        bValue = b.stats.damage.dealt / numRound;
+      } else {
+        column.split('.').forEach(key => {
+          aValue = aValue ? aValue[key] : 'N/A';
+          bValue = bValue ? bValue[key] : 'N/A';
+        });
+      }
+
+      if (aValue < bValue) return sortDirection === "asc" ? -1 : 1;
+      if (aValue > bValue) return sortDirection === "asc" ? 1 : -1;
+      return 0;
+    });
+
+    setData(sortedData);
+  };
+
+  const columns = [
+    { key: "name", label: "Player" },
+    { key: "acs", label: "ACS" },
+    { key: "stats.kills", label: "K" },
+    { key: "stats.deaths", label: "D" },
+    { key: "stats.assists", label: "A" },
+    { key: "stats.headshots", label: "HS" },
+    { key: "adr", label: "ADR" },
+    { key: "fk", label: "FK" },
+    { key: "mk", label: "MK" },
+  ];
+
+  const renderTable = (teamData, teamColor) => (
+    <div ref={tableRef} className="w-full overflow-x-auto shadow-lg rounded-lg mb-8">
+      <table className="w-full min-w-max table-auto font-bold text-center ">
+        <thead>
+          <tr className={`uppercase text-sm leading-normal sticky top-0`}>
+            {columns.map((column, index) => (
+              <th
+                key={column.key}
+                className={`py-[6px] bg-[#362431] px-2 text-center text-[10.5px] text-white cursor-pointer hover:bg-${teamColor}-200 transition-colors ${index === 0 ? "sticky left-0 z-10 bg-${teamColor}-100" : ""}`}
+                onClick={() => handleSort(column.key)}
+                style={index === 0 ? { width: '190px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' } : { width: '10px' }} // Set width for non-first columns
+              >
+                <div className="flex items-center justify-center">
+                  <span>{column.label}</span>
+                  <span className="ml-1">
+                    {sortColumn === column.key ? (
+                      sortDirection === "asc" ? (
+                        <FaSortUp className={`text-${teamColor}-500`} />
+                      ) : (
+                        <FaSortDown className={`text-${teamColor}-500`} />
+                      )
+                    ) : (
+                      <FaSort className={`text-${teamColor}-300`} />
+                    )}
+                  </span>
+                </div>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className={`text-[10.5px]`}>
+          {teamData.map((row, rowIndex) => (
+            <tr
+              key={rowIndex}
+              className={`border-${teamColor}-200 hover:bg-${teamColor}-100 transition-colors`}
+            >
+              {columns.map((column, columnIndex) => {
+                let cellData;
+
+                if (column.key === "acs") {
+                  cellData = (row.stats.score / numRound).toFixed(0);
+                } else if (column.key === "adr") {
+                  cellData = (row.stats.damage.dealt / numRound).toFixed(0);
+                } else {
+                  const columnKeys = column.key.split('.');
+                  cellData = row;
+                  columnKeys.forEach(key => {
+                    cellData = cellData ? cellData[key] : 'N/A';
+                  });
+                }
+
+                return (
+                  <td
+                    key={`${rowIndex}-${column.key}`}
+                    className={`py-2 px-3 text-center ${columnIndex === 0 ? "sticky left-0 z-10 bg-base-100" : ""}`}
+                    style={columnIndex === 0 ? { width: '150px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' } : { width: '50px' }} // Set width for non-first columns
+                  >
+                    {column.key === "name" ? (
+                      <div className="flex items-center">
+                        <img
+                          src={imageUrls[row.agent.name]}
+                          alt={row.agent.name}
+                          className="w-8 h-8 rounded-full mr-2"
+                        />
+                        <span>{row.name}#{row.tag}</span>
+                      </div>
+                    ) : (
+                      cellData
+                    )}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+
+  if (error) {
+    return <div className="text-red-500 text-center">Error: {error}</div>;
+  }
+
+  const redTeam = data.filter(player => player.team_id === "Red");
+  const blueTeam = data.filter(player => player.team_id === "Blue");
+
+  return (
+    <div className="w-full overflow-x-auto flex flex-col xl:flex-row gap-5">
+      <div className="w-full xl:w-[49%]">
+        <h3 className="text-xl font-bold mb-2 text-red-600">Red Team</h3>
+        {renderTable(redTeam, "red")}
+      </div>
+      <div className="w-full xl:w-[49%]">
+        <h3 className="text-xl font-bold mb-2 text-blue-600">Blue Team</h3>
+        {renderTable(blueTeam, "blue")}
+      </div>
+    </div>
+  );
 }
+
+export default FeatureRichTable;
