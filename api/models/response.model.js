d4a3f7e03c43e633c2549699be2c7f0142eee020
DongChuyenNghiep <tuan22000@student.hamk.fi>
2024-10-16 17:13:07 +0300

    fix api broken

--- api/models/response.model.js
+++ api/models/response.model.js
@@ -3,13 +3,13 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const questionResponseSchema = new Schema({
-  questionId: { type: Number, required: true },     // Question ID (e.g., 3, 4, 5, etc.)
-  selectedTeams: { type: [String], required: true } // Selected teams for this question
+  questionId: { type: Number, required: true },   
+  selectedTeams: { type: [String], required: true } 
 });
 
 const predictionSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  userId: { type: String },
   answers: { type: [questionResponseSchema], required: true },  // Array of question answers 
 });
-
-export default model('PredictionPickem', predictionSchema,'PredictionPickem');
+const PredictionPickem = mongoose.model('PredictionPickem', predictionSchema,'PredictionPickem');
+export default PredictionPickem;
