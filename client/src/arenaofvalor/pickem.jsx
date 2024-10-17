a91d18abec8adcf9231b4763cf9d786c1b1db746
DongChuyenNghiep <tuan22000@student.hamk.fi>
2024-10-17 16:41:16 +0300

    Update pickem.jsx

--- client/src/arenaofvalor/pickem.jsx
+++ client/src/arenaofvalor/pickem.jsx
@@ -156,7 +156,7 @@ const PickemChallenge = () => {
                   onClick={() => openModal(question)}
                 >
                   {predictions[question.id]?.length > 0 ? (
-                   <div className={`grid grid-cols-2 sm:grid-cols-2 ${question.maxChoose === 3 ? 'lg:grid-cols-3' : 'lg:grid-cols-4'} gap-2 lg:gap-x-8`}>
+                   <div className={`grid grid-cols-2 lg:grid-cols-${question.maxChoose} gap-2 lg:gap-x-8`}>
                       {predictions[question.id]?.map((team) => {
                         const selectedTeam = question.options.find((option) => option.name === team);
                         return (
