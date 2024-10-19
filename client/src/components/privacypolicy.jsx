ec5c97a4056de2857206059c5d4c431af9609776
DongChuyenNghiep <tuan22000@student.hamk.fi>
2024-10-20 01:57:33 +0300

    create tos

--- client/src/components/privacypolicy.jsx
+++ client/src/components/privacypolicy.jsx
@@ -5,12 +5,12 @@ const PrivacyPolicy = () => {
     <div className="bg-base-100 text-base-content lg:mt-20 mt-14  p-8 min-h-screen">
       <div className="max-w-5xl mx-auto">
         <h1 className="text-4xl font-bold mb-5 text-center">Chính Sách Bảo Mật</h1>
-        <p><strong>Ngày Hiệu Lực:</strong> 20 tháng 10, 2024</p>
+        <p><strong>Ngày Hiệu Lực:</strong> 15 tháng 10, 2024</p>
 
         <p>
-          Chúng tôi, App ghi nhận data giải đấu Valorant Dong Chuyen Nghiep ("ứng dụng", "chúng tôi", "của chúng tôi"), rất coi trọng quyền riêng tư của bạn. 
+          Chúng tôi, App ghi nhận data giải đấu Esport Dong Chuyen Nghiep ("ứng dụng", "chúng tôi", "của chúng tôi"), rất coi trọng quyền riêng tư của bạn. 
           Chính sách bảo mật này cung cấp chi tiết về cách chúng tôi thu thập, sử dụng, bảo vệ và lưu trữ thông tin cá nhân của bạn 
-          khi bạn sử dụng ứng dụng của chúng tôi. Ứng dụng này được phát triển và sử dụng để lưu trữ dữ liệu liên quan đến các giải đấu Valorant của trường THPT Phú Nhuận.
+          khi bạn sử dụng ứng dụng của chúng tôi. Ứng dụng này được phát triển và sử dụng để lưu trữ dữ liệu liên quan đến các giải đấu Valorant, Arena Of Valor, Teamfight Tactics, League Of Legend của Dong Chuyen Nghiep.
         </p>
 
         <h2 className="text-2xl font-semibold mt-6">1. Thông Tin Chúng Tôi Thu Thập</h2>
@@ -32,7 +32,7 @@ const PrivacyPolicy = () => {
         <h3 className="text-xl font-semibold mt-4">a. Cung Cấp và Cải Thiện Dịch Vụ</h3>
         <ul className="list-disc ml-6">
           <li>Tạo tài khoản và cung cấp quyền truy cập vào các tính năng của ứng dụng.</li>
-          <li>Lưu trữ và quản lý thông tin về giải đấu Valorant của Dong Chuyen Nghiep.</li>
+          <li>Lưu trữ và quản lý thông tin về giải đấu Valorant, Arena Of Valor, Teamfight Tactics, League Of Legend của Dong Chuyen Nghiep.</li>
           <li>Giám sát hoạt động giải đấu và cập nhật dữ liệu liên quan.</li>
         </ul>
 
@@ -40,7 +40,7 @@ const PrivacyPolicy = () => {
         <p>Chúng tôi sử dụng thông tin thu thập để phân tích cách người dùng tương tác với ứng dụng. Điều này giúp chúng tôi hiểu rõ các xu hướng sử dụng, phát hiện sự cố kỹ thuật, và cải thiện chức năng của ứng dụng.</p>
 
         <h2 className="text-2xl font-semibold mt-6">3. Chia Sẻ Thông Tin Của Bạn</h2>
-        <p>Chúng tôi cam kết không chia sẻ thông tin cá nhân của bạn dưới bất kỳ hình thức nào cho bên thứ ba. Thông tin của bạn chỉ được sử dụng nội bộ cho mục đích quản lý và điều hành các giải đấu Valorant của Dong Chuyen Nghiep. Chúng tôi cũng sẽ không bán, trao đổi hoặc chuyển giao thông tin của bạn cho bất kỳ bên thứ ba nào.</p>
+        <p>Chúng tôi cam kết không chia sẻ thông tin cá nhân của bạn dưới bất kỳ hình thức nào cho bên thứ ba. Thông tin của bạn chỉ được sử dụng nội bộ cho mục đích quản lý và điều hành các giải đấu Valorant, Arena Of Valor, Teamfight Tactics, League Of Legend của Dong Chuyen Nghiep. Chúng tôi cũng sẽ không bán, trao đổi hoặc chuyển giao thông tin của bạn cho bất kỳ bên thứ ba nào.</p>
 
         <h2 className="text-2xl font-semibold mt-6">4. Bảo Mật Dữ Liệu</h2>
         <p>Chúng tôi cam kết bảo mật thông tin cá nhân của bạn bằng cách áp dụng các biện pháp an ninh kỹ thuật hợp lý, bao gồm mã hóa. Tuy nhiên, xin lưu ý rằng không có phương pháp truyền tải qua Internet hoặc lưu trữ điện tử nào hoàn toàn an toàn. Dù chúng tôi cố gắng bảo vệ dữ liệu của bạn, nhưng không thể đảm bảo an ninh tuyệt đối.</p>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -29,6 +29,7 @@ import WelcomePage from './arenaofvalor/welcomepickem.jsx';
 import PickemChallengeMatch from './arenaofvalor/pickemmatch.jsx';
 import TournamentBracketAOV from './arenaofvalor/bracket.jsx'
 import PrivacyPolicy from './components/privacypolicy.jsx';
+import TermsOfService from './components/termofservice.jsx';
 // Check if the root element exists
 const rootElement = document.getElementById('root');
 
@@ -57,6 +58,7 @@ if (rootElement) {
               <Route path='/arenaofvalor/pickem/welcome' element={<WelcomePage />} />
               <Route path='/arenaofvalor/swisstage' element={<TournamentBracketAOV />} />
               <Route path='/privacy' element={<PrivacyPolicy />} />
+              <Route path='/tos' element={<TermsOfService />} />
               <Route element={<PrivateRoute />}>
               <Route path='/arenaofvalor/pickem/pickemmatch' element={<PickemChallengeMatch />} />
                 <Route path='/arenaofvalor/register' element={<TeamRegistrationFormAOV />} />
