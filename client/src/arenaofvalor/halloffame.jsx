101db263c288d97c88e93d20a26b25a8445994bc
DongChuyenNghiep <tuan22000@student.hamk.fi>
2024-10-27 14:01:16 +0200

    Update halloffame.jsx

--- client/src/arenaofvalor/halloffame.jsx
+++ client/src/arenaofvalor/halloffame.jsx
@@ -67,20 +67,20 @@ const TeamPageHOF = () => {
                     <div className="flex items-center space-x-4">
                         {team.rank <= 3 && (
                             <div className="relative mr-4">
-                                <FaTrophy className={`text-2xl ${team.rank === 1 ? "text-yellow-400" : team.rank === 2 ? "text-gray-400" : "text-orange-400"}`} />
+                                <FaTrophy className={`text-4xl ${team.rank === 1 ? "text-yellow-400" : team.rank === 2 ? "text-gray-400" : "text-orange-400"}`} />
                                 <span className="absolute top-0 right-0 -mt-1 -mr-1 bg-white text-black rounded-full text-xs font-bold px-1">{team.rank}</span>
                             </div>
                         )}
                         <img
                             src={`https://drive.google.com/thumbnail?id=${team.logo}`}
                             alt={`${team.name} logo`}
-                            className="w-20 h-20  object-cover"
+                            className="lg:w-20 lg:h-20 w-14 h-14 object-cover"
                         />
-                        <h3 className="text-2xl font-bold">{team.name}</h3>
+                        <h3 className="lg:text-2xl text-[15px] font-bold">{team.name}</h3>
                     </div>
                 </div>
 
-                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-x-3">
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-x-3 gap-y-1">
                     {team.players.map((player) => (
                         <div
                             key={player._id}
@@ -91,9 +91,9 @@ const TeamPageHOF = () => {
                             <img
                                 src={`https://drive.google.com/thumbnail?id=${player.avatar}`}
                                 alt={player.name}
-                                className="w-20 h-20 rounded-full object-cover"
+                                className="lg:w-20 lg:h-20 w-14 h-14 rounded-full object-cover"
                             />
-                            <span className="font-medium text-center mt-4">{player.name}</span>
+                            <span className="font-medium text-[15px] text-center mt-4">{player.name}</span>
                         </div>
                     ))}
                 </div>
@@ -138,7 +138,7 @@ const TeamPageHOF = () => {
             </div>
 
             {/* Main Content */}
-            <div className="lg:w-[80%] py-12 px-4 sm:px-6 lg:px-8 lg:ml-[20%] w-full">
+            <div className="lg:w-[80%] py-12 px-4 sm:px-6 lg:px-8 lg:mt-0 md:mt-10 lg:ml-[20%] w-full">
                 <div className="max-w-[1400px] mx-auto">
                     <h1 className="text-4xl font-bold text-center mb-12 mt-16 md:mt-0">Team Hall of Fame</h1>
 
