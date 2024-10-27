2f95df0b7cd66a83471a48948d51d448b628bc0f
DongChuyenNghiep <tuan22000@student.hamk.fi>
2024-10-27 12:30:19 +0200

    Update teamhof.model.js

--- api/models/teamhof.model.js
+++ api/models/teamhof.model.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const playerSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  avatar: { type: String, required: true },
+  avatar: { type: String, required: true,default:"1wRTVjigKJEXt8iZEKnBX5_2jG7Ud3G-L" },
 });
 
 const teamSchema = new mongoose.Schema({
   game:{type:String,required:true},
   name: { type: String, required: true },
-  logo: { type: String, required: true,default:"1wRTVjigKJEXt8iZEKnBX5_2jG7Ud3G-L" },
+  logo: { type: String, required: true },
   color: { type: String, required: true },
   players: [playerSchema],
   league: { type: String, required: true},
