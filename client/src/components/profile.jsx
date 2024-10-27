820a01516c9a652c73f5319edae028d53fe93b20
DongChuyenNghiep <tuan22000@student.hamk.fi>
2024-10-28 00:47:09 +0200

    Update profile.jsx

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -94,7 +94,6 @@ export default function Profile() {
                     <input type="text" onChange={handleChange} defaultValue={currentUser.riotID} id='riotID' placeholder="Riot ID" className="p-3 my-[6px] rounded-lg border-primary border-[1.5px]" />
                     <input type="text" onChange={handleChange} defaultValue={currentUser.discordID} id='discordID' placeholder="Discord ID" className="p-3 my-[6px] rounded-lg border-primary border-[1.5px]" />
                     <input type="text" onChange={handleChange} defaultValue={currentUser.nickname} id='nickname' placeholder="Nickname" className="p-3 my-[6px] rounded-lg border-primary border-[1.5px]" />
-                    <input type="text" onChange={handleChange} defaultValue={currentUser.username} id='username' placeholder="Username" className="p-3 my-[6px] rounded-lg border-primary border-[1.5px]" />
                     <input type="password" onChange={handleChange} id='password' placeholder="Password" className="p-3 my-[6px] rounded-lg border-primary border-[1.5px]" />
                     <button className="btn mt-3 bg-primary hover:bg-neutral text-white"> {loading ? 'Loading...' : 'Update'}</button>
                 </form>
