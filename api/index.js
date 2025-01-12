06171ee7973b833f429c53dd097df2639c6e12e9
DongChuyenNghiep <tuan22000@student.hamk.fi>
2025-01-12 21:18:04 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -76,7 +76,7 @@ app.get('/oauth2-callback', function(req, res) {
       };
 
       // legibly print out our tokens
-      res.send("<pre>" + JSON.stringify(tokens, false, 4) + "</pre>");
+      res.send("<pre>" + JSON.stringify(payload, false, 4) + "</pre>");
   }else {
     res.send("/token request failed");
 }
