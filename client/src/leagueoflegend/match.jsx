2b4c4654b480a1a3e4bba9453d2d0125c3cc3d30
DongChuyenNghiep <tuan22000@student.hamk.fi>
2025-01-15 18:46:22 +0200

    Update match.jsx

--- client/src/leagueoflegend/match.jsx
+++ client/src/leagueoflegend/match.jsx
@@ -1,28 +1,37 @@
 import { useState, useEffect } from "react";
 
 export default function MatchStatLOL() {
-    const [rawData, setRawData] = useState(null);
+    const [rawData, setRawData] = useState({});
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
-    const matchId = "VN2_637421825";
+    const matchIds = ["VN2_637421825", "VN2_724137610"];
 
     // Fetch match data
     useEffect(() => {
-        fetch(`https://dongchuyennghiep-backend.vercel.app/api/lol/match/${matchId}`)
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error("Failed to fetch data");
-                }
-                return res.json();
-            })
-            .then((data) => {
-                setRawData(data);
+        const fetchAllMatches = async () => {
+            try {
+                const matchPromises = matchIds.map((matchId) =>
+                    fetch(`https://dongchuyennghiep-backend.vercel.app/api/lol/match/${matchId}`).then((res) => {
+                        if (!res.ok) {
+                            throw new Error(`Failed to fetch data for matchId: ${matchId}`);
+                        }
+                        return res.json();
+                    })
+                );
+                const results = await Promise.all(matchPromises);
+                const matchData = {};
+                matchIds.forEach((id, index) => {
+                    matchData[id] = results[index];
+                });
+                setRawData(matchData);
                 setIsLoading(false);
-            })
-            .catch((err) => {
+            } catch (err) {
                 setError(err.message);
                 setIsLoading(false);
-            });
+            }
+        };
+
+        fetchAllMatches();
     }, []);
 
     // Import all images
@@ -31,16 +40,15 @@ export default function MatchStatLOL() {
 
         directories.forEach((directory) => {
             let imageContext;
-            if (directory === '../item') {
-                imageContext = import.meta.glob('../item/*.png', { eager: true });
-            } else if (directory === '../championLOL') {
-                imageContext = import.meta.glob('../championLOL/*.png', { eager: true });
-            }
-            else if (directory === '../LOLrole') {
-                imageContext = import.meta.glob('../LOLrole/*.png', { eager: true });
+            if (directory === "../item") {
+                imageContext = import.meta.glob("../item/*.png", { eager: true });
+            } else if (directory === "../championLOL") {
+                imageContext = import.meta.glob("../championLOL/*.png", { eager: true });
+            } else if (directory === "../LOLrole") {
+                imageContext = import.meta.glob("../LOLrole/*.png", { eager: true });
             }
             for (const path in imageContext) {
-                const fileName = path.split('/').pop().replace('.png', '');
+                const fileName = path.split("/").pop().replace(".png", "");
                 images[fileName] = imageContext[path].default || imageContext[path];
             }
         });
@@ -49,7 +57,7 @@ export default function MatchStatLOL() {
     };
 
     // Import from multiple directories
-    const itemImages = importAllImages(['../item', '../championLOL', '../LOLrole']);
+    const itemImages = importAllImages(["../item", "../championLOL", "../LOLrole"]);
 
     if (isLoading) {
         return (
@@ -63,36 +71,19 @@ export default function MatchStatLOL() {
         return <div>Error: {error}</div>;
     }
 
-    const participants = rawData.info.participants;
-    const gameDurationSeconds = rawData.info.gameDuration;
-    const gameDurationMinutes = gameDurationSeconds / 60;
-
-    // Define the priority order for team positions
-    const teamPositionOrder = ['TOP', 'JUNGLE', 'MIDDLE', 'BOTTOM', 'UTILITY'];
-    const sortByTeamPosition = (players) => {
-        return players.sort(
-            (a, b) => teamPositionOrder.indexOf(a.teamPosition) - teamPositionOrder.indexOf(b.teamPosition)
-        );
-    };
-
-    // Tách người chơi theo teamId
-    const team1 = sortByTeamPosition(participants.filter((player) => player.teamId === 100));
-    const team2 = sortByTeamPosition(participants.filter((player) => player.teamId === 200));
-
-    const renderTable = (team) => (
+    const renderTable = (team, gameDurationMinutes) => (
         <div className="table-container p-2">
-            {/* Wrapper for responsiveness */}
             <div className="overflow-x-auto">
                 <table className="min-w-full table-auto text-base-content lg:w-full w-[280%] font-semibold">
-                    <thead>
+                    <thead className="bg-[#362431]">
                         <tr>
-                            <th className="p-2 border-b border-gray-700 sticky left-0 bg-base-100 z-10 lg:!w-[270px] !w-[180px]">Player</th>
-                            <th className="p-2 border-b border-gray-700">Trang bị</th>
-                            <th className="p-2 border-b border-gray-700">KDA</th>
-                            <th className="p-2 border-b border-gray-700">CS</th>
-                            <th className="p-2 border-b border-gray-700">Vàng đã nhận</th>
-                            <th className="p-2 border-b border-gray-700">Điểm Tầm Nhìn</th>
-                            <th className="p-2 border-b border-gray-700">Sát Thương</th>
+                            <th className="p-2 bg-[#362431] sticky left-0 z-10 lg:!w-[270px] !w-[180px]">Player</th>
+                            <th className="p-2  ">Trang bị</th>
+                            <th className="p-2  ">KDA</th>
+                            <th className="p-2  ">CS</th>
+                            <th className="p-2  ">Vàng đã nhận</th>
+                            <th className="p-2  ">Điểm Tầm Nhìn</th>
+                            <th className="p-2  ">Sát Thương</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -122,20 +113,18 @@ export default function MatchStatLOL() {
                             } = participant;
 
                             return (
-                                <tr key={puuid}> {/* Đặt chiều cao hàng */}
-                                    <td className="!h-[10px] border-b border-gray-700 lg:!w-[270px] !w-[180px] sticky left-0 bg-base-100 z-10">
+                                <tr key={puuid} className="!h-[45.2px]">
+                                    <td className="   lg:!w-[270px] !w-[180px] sticky left-0 bg-base-100 z-10">
                                         <div className="flex flex-row gap-x-3">
                                             <img src={itemImages[teamPosition]} alt={`${teamPosition}`} className="inline-block h-7 w-7 rounded" />
                                             <img src={itemImages[championName]} alt={`Item ${championName}`} className="inline-block h-7 w-7 rounded" />
                                             <span className="flex items-center">
-                                                {/* Hiển thị đầy đủ khi màn hình >= 1024px */}
                                                 <span className="hidden lg:inline text-[12px]">{riotIdGameName}#{riotIdTagline}</span>
-                                                {/* Chỉ hiển thị riotIdGameName khi màn hình < 1024px */}
                                                 <span className="lg:hidden text-[12px]">{riotIdGameName}</span>
                                             </span>
                                         </div>
                                     </td>
-                                    <td className="p-2 border-b border-gray-700 flex gap-1 items-center justify-center">
+                                    <td className="p-2  flex gap-1 items-center justify-center">
                                         {[item0, item1, item2, item3, item4, item5, item6].map((item, index) => {
                                             const imgSrc = itemImages[item];
                                             return imgSrc ? (
@@ -145,7 +134,7 @@ export default function MatchStatLOL() {
                                             );
                                         })}
                                     </td>
-                                    <td className=" text-[13px] p-2 border-b border-gray-700 text-center">
+                                    <td className=" text-[13px] p-2   text-center">
                                         {kills}/{deaths}/{assists}{" "}
                                         {challenges?.visionScorePerMinute && (
                                             <span>
@@ -153,12 +142,12 @@ export default function MatchStatLOL() {
                                             </span>
                                         )}
                                     </td>
-                                    <td className=" text-[13px] p-2 border-b border-gray-700 text-center">
+                                    <td className=" text-[13px] p-2   text-center">
                                         {totalMinionsKilled + neutralMinionsKilled} (
                                         {((totalMinionsKilled + neutralMinionsKilled) / gameDurationMinutes).toFixed(1)})
                                     </td>
-                                    <td className="text-[13px] p-2 border-b border-gray-700 text-center">{goldEarned}</td>
-                                    <td className="text-[13px] p-2 border-b border-gray-700 text-center">
+                                    <td className="text-[13px] p-2   text-center">{goldEarned}</td>
+                                    <td className="text-[13px] p-2   text-center">
                                         {visionScore}{" "}
                                         {challenges?.visionScorePerMinute && (
                                             <span>
@@ -166,7 +155,7 @@ export default function MatchStatLOL() {
                                             </span>
                                         )}
                                     </td>
-                                    <td className="text-[13px] p-2 border-b border-gray-700 text-center">{totalDamageDealtToChampions}</td>
+                                    <td className="text-[13px] p-2   text-center">{totalDamageDealtToChampions}</td>
                                 </tr>
                             );
                         })}
@@ -176,17 +165,34 @@ export default function MatchStatLOL() {
         </div>
     );
 
-
-
-
     return (
         <div className="matchstat">
             <div className="title bg-[#362431] text-white p-4">
                 <h2 className="text-xl">League of Legends Match Stats</h2>
-                <p>Nhánh 0-0 ● BO1</p>
             </div>
-            {renderTable(team1)}
-            {renderTable(team2)}
+            {matchIds.map((matchId) => {
+                const data = rawData[matchId]?.info;
+                if (!data) return null;
+
+                const participants = data.participants;
+                const gameDurationSeconds = data.gameDuration;
+                const gameDurationMinutes = gameDurationSeconds / 60;
+
+                const teamPositionOrder = ["TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"];
+                const sortByTeamPosition = (players) =>
+                    players.sort((a, b) => teamPositionOrder.indexOf(a.teamPosition) - teamPositionOrder.indexOf(b.teamPosition));
+
+                const team1 = sortByTeamPosition(participants.filter((player) => player.teamId === 100));
+                const team2 = sortByTeamPosition(participants.filter((player) => player.teamId === 200));
+
+                return (
+                    <div key={matchId}>
+                        <h3 className="text-lg font-bold mt-4">Match ID: {matchId}</h3>
+                        {renderTable(team1, gameDurationMinutes)}
+                        {renderTable(team2, gameDurationMinutes)}
+                    </div>
+                );
+            })}
         </div>
     );
 }
