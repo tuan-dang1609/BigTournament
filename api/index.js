4e6c072315353327054af14353c37613ca125c00
DongChuyenNghiep <tuan22000@student.hamk.fi>
2025-03-25 21:42:28 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -298,7 +298,7 @@ app.get('/api/valorant/match/:matchId', async (req, res) => {
       blueTeam.sort((a, b) => (b.stats?.acs ?? 0) - (a.stats?.acs ?? 0));
 
       // Gộp lại
-      matchData.players = [...redTeam, ...blueTeam];
+      matchData.players = [ ...blueTeam,...redTeam];
     }
 
     res.setHeader('Access-Control-Allow-Origin', '*');
