c0fba32a5f7f4a9ddaf7383244fb35f2c32def06
DongChuyenNghiep <tuan22000@student.hamk.fi>
2025-03-29 12:54:31 +0200

    add socket.io

--- client/src/valorant/test2.jsx
+++ client/src/valorant/test2.jsx
@@ -9,7 +9,7 @@ export default function MatchInterface() {
   const socketRef = useRef(null); // 🆕
 
   useEffect(() => {
-    const socket = io('http://localhost:3000');
+    const socket = io('https://dongchuyennghiep-backend.vercel.app/');
     socketRef.current = socket;
 
     socket.on('connect', () => {
@@ -31,7 +31,7 @@ export default function MatchInterface() {
   useEffect(() => {
     const fetchMatch = async () => {
       try {
-        const res = await fetch('http://localhost:3000/api/auth/status', {
+        const res = await fetch('https://dongchuyennghiep-backend.vercel.app/api/auth/status', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ matchId })
@@ -60,7 +60,7 @@ export default function MatchInterface() {
     });
 
     try {
-      await fetch('http://localhost:3000/api/auth/action', {
+      await fetch('https://dongchuyennghiep-backend.vercel.app/api/auth/action', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ matchId, action, role, ...data })
