f5947a3fc8df315c514feb70ac2c918f05c10000
DongChuyenNghiep <tuan22000@student.hamk.fi>
2025-06-28 17:01:26 +0300

    Update user.model.js

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,62 +1,62 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const teamSchema = new mongoose.Schema(
-  {name:{
-    type:String
-  },
-   logoTeam: String,
-   shortName: String 
+  {
+    name: {
+      type: String,
+    },
+    logoTeam: String,
+    shortName: String,
   },
   { timestamps: true }
 );
 const userSchema = new mongoose.Schema(
-  {nickname:{
-    type:String,
-    required: true,
-    default:"TBD"
-  },
-    discordID:{
-      type:String,
-      required:true,
-      default:""
-    },
-    className:{
-      type:String,
-      required:true,
-      default:""
-    },
-    garenaaccount:{
-      type:String,
+  {
+    nickname: {
+      type: String,
       required: true,
-      default:"TBD"
+      default: "TBD",
     },
-    riotID:{
-      type:String,
-      default:""
+    discordID: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    className: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    garenaaccount: {
+      type: String,
+      default: "TBD",
+    },
+    riotID: {
+      type: String,
+      default: "",
     },
     username: {
       type: String,
       required: true,
       unique: true,
     },
- 
+
     password: {
       type: String,
       required: true,
     },
     profilePicture: {
       type: String,
-      default:
-        '1wRTVjigKJEXt8iZEKnBX5_2jG7Ud3G-L',
+      default: "1wRTVjigKJEXt8iZEKnBX5_2jG7Ud3G-L",
     },
     team: {
-      type: teamSchema,   // ✅ Chỉ là một object, không phải array
-      default: {}
-    }
+      type: teamSchema, // ✅ Chỉ là một object, không phải array
+      default: {},
+    },
   },
   { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema,'users');
+const User = mongoose.model("User", userSchema, "users");
 
 export default User;
